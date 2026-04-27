6b3dc15a154b894d07ea0e4af4c1c98cec711265
Aditya Jain <adityajain@Aditya-MacAir.local>
2026-04-27 15:18:28 +0530

    scrollable image chage

--- src/pages/NewPages/HomePage.jsx
+++ src/pages/NewPages/HomePage.jsx
@@ -16,6 +16,28 @@ import Header from './Header';
 import { HomePagePrepoComponent } from './HomePagePrepoComponent';
 import { FeaturesGrid } from './FeaturesGrid';
 import { motion } from 'framer-motion';
+import img1 from "../../assets/images/updated-img-logo/1.png";
+import img2 from "../../assets/images/updated-img-logo/2.png";
+import img3 from "../../assets/images/updated-img-logo/3.png";
+import img4 from "../../assets/images/updated-img-logo/4.png";
+import img5 from "../../assets/images/updated-img-logo/5.png";
+import img6 from "../../assets/images/updated-img-logo/6.png";
+import img7 from "../../assets/images/updated-img-logo/7.png";
+import img8 from "../../assets/images/updated-img-logo/8.png";
+import img9 from "../../assets/images/updated-img-logo/9.png";
+import img10 from "../../assets/images/updated-img-logo/10.png";
+import img11 from "../../assets/images/updated-img-logo/11.png";
+import img12 from "../../assets/images/updated-img-logo/12.png";
+import img13 from "../../assets/images/updated-img-logo/13.png";
+import img14 from "../../assets/images/updated-img-logo/14.png";
+import img15 from "../../assets/images/updated-img-logo/15.png";
+import img16 from "../../assets/images/updated-img-logo/16.png";
+import img17 from "../../assets/images/updated-img-logo/17.png";
+import img18 from "../../assets/images/updated-img-logo/18.png";
+import img19 from "../../assets/images/updated-img-logo/19.png";
+import img20 from "../../assets/images/updated-img-logo/20.png";
+import img21 from "../../assets/images/updated-img-logo/21.png";
+import img22 from "../../assets/images/updated-img-logo/22.png";
 
 const courses = [
   {
@@ -57,13 +79,30 @@ const courses = [
 ];
 
 const brands = [
-  images.newHomePageBrandsImage1,
-  images.newHomePageBrandsImage2,
-  images.newHomePageBrandsImage3,
-  images.newHomePageBrandsImage4,
-  images.newHomePageBrandsImage5,
-  images.newHomePageBrandsImage6,
+  img1,
+  img2,
+  img3,
+  img4,
+  img5,
+  img6,
+  img7,
+  img8,
+  img9,
+  img10,
+  img11,
+  img12,
+  img13,
+  img14,
+  img15,
+  img16,
+  img17,
+  img18,
+  img19,
+  img20,
+  img21,
+  img22, 
 ];
+
 const howWeWork = [
   {
     icon: 'heroicons-solid:eye',
@@ -378,12 +417,12 @@ const HomePage = () => {
             </p>
             <div className="overflow-hidden w-full mt-4">
               <div className="flex w-max animate-scroll gap-3">
-                {[...brands, ...brands, ...brands].map((brand, index) => (
+                {[...brands, ...brands].map((brand, index) => (
                   <div
                     key={index}
-                    className="flex items-center justify-center px-6 border rounded-lg border-gray-300"
+                    className="flex items-center justify-center px-6 py-2 border rounded-lg border-gray-300"
                   >
-                    <img src={brand} className="w-24 h-14 object-contain" />
+                    <img src={brand} className="h-12 object-cover" />
                   </div>
                 ))}
               </div>
